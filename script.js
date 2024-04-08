30c75fc02c12d9440887ab311d027bcaa4697206
Alice <alicesb2006@gmail.com>
2024-04-08 09:01:16 -0300

    Criando javascript com array, objeto e templaits str

--- script.js
+++ script.js
@@ -34,3 +34,18 @@ for(let num of lista_num){
 
 console.log(pares);
 console.log(impares);
+
+/*Com uma lista de alunos >>>(Objeto)<<< vão percorrer cada aluno e trazer a 
+  média da nota e mostra o nome do aluno e sua media*/
+
+const lista_alunos = [
+    {nome: "Lisan Al-Gaib", nota: [10,9]},
+    {nome: "Paul Atreides", nota: [8.9,9]},
+    {nome: "Ivan", nota: [8,9]},
+    {nome: "Till", nota: [7.0,8.9]}
+];
+
+for(aluno of lista_alunos){
+    let media = (aluno.nota[0] + aluno.nota[1])/ 2;
+    console.log(`A média do aluno ${aluno.nome} é ${media}`)
+}
